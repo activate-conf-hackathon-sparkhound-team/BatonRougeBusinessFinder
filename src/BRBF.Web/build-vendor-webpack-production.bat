613db77c419ac328c480d7a916ec0e99251e6890
John Harvey <john.harvey@sparkhound.com>
2018-02-23 14:43:43 -0600

    Update app for publishing in Azure.

---
+++ src/BRBF.Web/build-vendor-webpack-production.bat
@@ -0,0 +1,2 @@
+node node_modules/webpack/bin/webpack.js --config webpack.config.vendor.js --env.prod
+node node_modules/webpack/bin/webpack.js --config webpack.config.js --env.prod
--- src/BRBF.Web/webpack.config.js
+++ src/BRBF.Web/webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-const AotPlugin = require('@ngtools/webpack').AotPlugin;
+//const AotPlugin = require('@ngtools/webpack').AotPlugin;
+const AngularCompilerPlugin = require('@ngtools/webpack').AngularCompilerPlugin;
 const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin;
 
 module.exports = (env) => {
@@ -20,7 +21,8 @@ module.exports = (env) => {
                 { test: /\.ts$/, use: isDevBuild ? ['awesome-typescript-loader?silent=true', 'angular2-template-loader', 'angular2-router-loader'] : '@ngtools/webpack' },
                 { test: /\.html$/, use: 'html-loader?minimize=false' },
                 { test: /\.css$/, use: [ 'to-string-loader', isDevBuild ? 'css-loader' : 'css-loader?minimize' ] },
-                { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
+                { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' },
+                { test: /(?:\.ngfactory\.js|\.ngstyle\.js|\.ts)$/, loader: '@ngtools/webpack' }
             ]
         },
         plugins: [new CheckerPlugin()]
@@ -44,11 +46,17 @@ module.exports = (env) => {
             })
         ] : [
             // Plugins that apply in production builds only
-            new webpack.optimize.UglifyJsPlugin(),
-            new AotPlugin({
+                //new webpack.optimize.UglifyJsPlugin(),
+                //new AotPlugin({
+                //    tsConfigPath: './tsconfig.json',
+                //    entryModule: path.join(__dirname, 'ClientApp/app/app.module#AppModule'),
+                //    exclude: ['./**/*.server.ts']
+                //})
+            new AngularCompilerPlugin({
                 tsConfigPath: './tsconfig.json',
                 entryModule: path.join(__dirname, 'ClientApp/app/app.module#AppModule'),
-                exclude: ['./**/*.server.ts']
+                exclude: ['./**/*.server.ts'],
+                sourceMap: true
             })
         ])
     });
